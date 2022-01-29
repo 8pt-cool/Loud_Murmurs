fa9d50bcf16c866a6e5692fd5af8d4a113224b13
8pt-cool <55686709+8pt-cool@users.noreply.github.com>
2022-01-29 19:47:10 +0800

    v1.1

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import html
 
-xml_file = open("rss.xml", encoding="utf-8")
+xml_file = open("part.xml", encoding="utf-8")
 html = xml_file.read()
 soup = BeautifulSoup(html,'html.parser')
 for child in soup.descendants:
     if child.name == 'item':
         filename=(child.title.next.strip()+".mp3").replace(' ','_')
+        filename=filename.replace('/','_')
         #print(filename)
         audio_link=child.enclosure['url']
         print(audio_link)
